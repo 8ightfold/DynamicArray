31fc9ba570c0e216f18af825191b74da64b779a4
eightfold <97198162+8ightfold@users.noreply.github.com>
2022-04-20 23:09:46 -0400

    Major code refactoring
    Basically rewrote the whole thing, removed the reserve function because I realized it was completely pointless

--- DynamicArray.hpp
+++ DynamicArray.hpp
@@ -4,9 +4,7 @@
 #include <cstdio>
 #include <memory>
 #include <cstring>
-
-typedef signed long sint;
-typedef unsigned long uint;
+#include <assert.h>
 
 template <class T>
 class dynArray {
@@ -14,289 +12,141 @@ private:
 
 	//~~~~~[MEMBERS]~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-	uint m_size;					//	"Public" size
-	T* m_basePtr;					//	The array
-	uint m_trueSize;				//	Size of the underlying array
-	bool m_reserved; uint m_idx;	//	Info used when reserving space
+	const char* m_name;
+	size_t m_size;					//	"Public" size
+	size_t m_trueSize;				//	Size of the underlying array
+	T* m_basePtr;					//	The underlying array
 
 public:
 
 	//~~~~~[FUNCTIONS]~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 	//-----[Constructors/Destructors]-------------------------------------------------------------------
 
-	dynArray() : m_basePtr(nullptr), m_size(0), m_trueSize(0), m_reserved(false), m_idx(0) {}
-	dynArray(int length, bool t = false) :	m_basePtr(nullptr), m_size(0), m_trueSize(0),  m_idx(0) {			
-		
-		this->m_reserved = t;	//	Regular if false, reserves if true
-		if ((uint)length == 0) {}
-		else if (!doAlloc(this, (uint)length)) printf("Memory allocation failed.\n");
-
+	dynArray() : m_basePtr(nullptr), m_size(0), m_trueSize(0) {}
+	dynArray(int g_size)
+		: m_basePtr(nullptr), m_size(0), m_trueSize(0)
+	{
+		if (!doAlloc(this, g_size)) printf("Memory allocation failed.");
 	}
 	template <size_t N>
-	dynArray(T(&r)[N]) : m_basePtr(nullptr), m_size(0), m_trueSize(0), m_reserved(false), m_idx(0) {
+	dynArray(T(&r)[N])  : m_basePtr(nullptr), m_size(0), m_trueSize(0) {
 		this->append(r);
 	};
 	template <size_t N>
-	dynArray(T(&&r)[N]) : m_basePtr(nullptr), m_size(0), m_trueSize(0), m_reserved(false), m_idx(0) {
+	dynArray(T(&&r)[N]) : m_basePtr(nullptr), m_size(0), m_trueSize(0) {
 		this->append(r);
 	};
 	~dynArray() { if (this->m_basePtr) free(this->m_basePtr); }
 
 	//-----[Size functions]-----------------------------------------------------------------------------
 
-	uint size() {
+	size_t size() { return this->m_size; }
 
-		if (this->m_reserved) return this->m_idx;
-		return this->m_size;
+	void resize(size_t n_size, bool clamp = false) {
 
-	}
-
-	//	Calling this will cancel out any previous reserve calls
-	//	All clamped data will be lost
-	void resize(int i, bool clamp = false) {
-
-		if (this->m_size == i) return;
-
-		if (this->m_reserved) {
-			this->m_size = this->m_idx;
-			this->m_reserved = false;
-		}
-
-		if (!doAlloc(this, (uint)i, clamp)) return;
-
-	}
-
-	//	"Reserves" extra space
-	void reserve(int i) {
-
-		if (i == 0) return;
-
-		if (this->m_reserved) {}
-		else this->m_idx = this->m_size;
-
-		if (!doAlloc(this, this->length + (uint)i)) return;
-		this->m_reserved = true;
+		if (!doResize(this, n_size, clamp)) printf("Resize failed.");
 
 	}
 
 	//-----[Stack-like functions]-----------------------------------------------------------------------
 
-	void push(T& t) {
+	void push(T t) {
 
-		if (this->m_reserved) {
-
-			++this->m_idx;
-			if (!doAlloc(this, this->m_idx)) return;
-			if (this->m_idx >= this->m_size) this->m_reserved = false;
-			this->m_basePtr[this->m_idx - 1] = t;
-			return;
-
-		}
-
-		if (!doAlloc(this, this->m_size + 1)) return;
-		this->m_basePtr[this->m_size - 1] = t;
-
-	}
-	void push(T&& t) {
-
-		if (this->m_reserved) {
-
-			++this->m_idx;
-			if (!doAlloc(this, this->m_idx)) return;
-			if (this->m_idx >= this->m_size) this->m_reserved = false;
-			this->m_basePtr[this->m_idx - 1] = t;
-			return;
-
-		}
-
-		if (!doAlloc(this, this->m_size + 1)) return;
+		if (!doAlloc(this, this->m_size + 1)) { printf("Memory allocation failed."); return; }
 		this->m_basePtr[this->m_size - 1] = t;
 
 	}
 
 	T pop() {
 
-		if (!this->m_basePtr || this->m_size == 0) { printf("Popping from empty array."); exit(0); }
-		if (this->m_reserved) {
-			--this->m_idx;
-			return this->m_basePtr[this->m_idx];
-		}
+		assert(this->m_size != 0);
 		--this->m_size;
 		return this->m_basePtr[this->m_size];
 
 	}
 
-	template<size_t N>
-	void append(const T(&r)[N]) {
+	void append(T& t)  { this->push(t); }
+	void append(T&& t) { this->push(t); }
+	void append(dynArray& r) {
 
-		unsigned long oldLength;
-		if (N == 0) return;
-
-		if (!this->m_basePtr) {
-
-			if (!doAlloc(this, N)) return;
-			if (!this->m_basePtr) return;
-			memcpy((void*)this->m_basePtr, (void*)r, sizeof(T) * N);
-			return;
-
-		}
-		
-		if (this->m_reserved) {
-			
-			oldLength = this->m_idx;
-			this->m_idx = this->m_idx + N;
-			if(!doAlloc(this, this->m_idx)) return;
-			if (this->m_idx >= this->m_size) this->m_reserved = false;
-			memcpy((void*)&this->m_basePtr[oldLength], (void*)r, sizeof(T) * N);
-			return;
-
-		}
-
-		oldLength = this->m_size;
-		if (!doAlloc(this, this->m_size + N)) return;
-		memcpy((void*)&this->m_basePtr[oldLength], (void*)r, sizeof(T) * N);
-
-	};
-	template<size_t N>
-	void append(const T(&&r)[N]) {
-
-		unsigned long oldLength;
-		if (N == 0) return;
-
-		if (!this->m_basePtr) {
-
-			if (!doAlloc(this, N)) return;
-			if (!this->m_basePtr) return;
-			memcpy((void*)this->m_basePtr, (void*)r, sizeof(T) * N);
-			return;
-
-		}
-
-		if (this->m_reserved) {
-
-			oldLength = this->m_idx;
-			this->m_idx = this->m_idx + N;
-			if (!doAlloc(this, this->m_idx)) return;
-			if (this->m_idx >= this->m_size) this->m_reserved = false;
-			memcpy((void*)&this->m_basePtr[oldLength], (void*)r, sizeof(T) * N);
-			return;
-
-		}
-
-		oldLength = this->m_size;
-		if (!doAlloc(this, this->m_size + N)) return;
-		memcpy((void*)&this->m_basePtr[oldLength], (void*)r, sizeof(T) * N);
-
-	};
-
-	void append(dynArray const& r) {
-
-		unsigned long oldLength;
 		if (this->m_basePtr == r.m_basePtr || !r.m_basePtr) return;
 
-		if (!this->m_basePtr) {
-
-			if(!doAlloc(this, r.m_size)) return;
-			memcpy((void*)this->m_basePtr, (void*)r.m_basePtr, r.m_size * sizeof(T));
-			return;
-
-		}
-
-		if (this->m_reserved) {
-
-			oldLength = this->m_idx;
-			this->m_idx = this->m_idx + r.m_size;
-			if (!doAlloc(this, this->m_idx)) return;
-			if (this->m_idx >= this->m_size) this->m_reserved = false;
-			memcpy((void*)&this->m_basePtr[oldLength], (void*)r.m_basePtr, r.m_size * sizeof(T));
-			return;
-
-		}
-
-		oldLength = this->m_size;
+		size_t o_size = this->m_size;
 		if (!doAlloc(this, this->m_size + r.m_size)) return;
-		memcpy((void*)&this->m_basePtr[oldLength], (void*)r.m_basePtr, r.m_size * sizeof(T));
+		memcpy((void*)&this->m_basePtr[o_size], (void*)r.m_basePtr, r.m_size * sizeof(T));
 
 	}
+	void append(dynArray&& r) {
+
+		if (this->m_basePtr == r.m_basePtr || !r.m_basePtr) return;
+
+		size_t o_size = this->m_size;
+		if (!doAlloc(this, this->m_size + r.m_size)) return;
+		memcpy((void*)&this->m_basePtr[o_size], (void*)r.m_basePtr, r.m_size * sizeof(T));
+
+	}
+	template <size_t N>
+	void append(T(&r)[N])  {
+
+		if (this->m_basePtr == r) return;
+
+		size_t o_size = this->m_size;
+		if (!doAlloc(this, this->m_size + N)) return;
+		memcpy((void*)&this->m_basePtr[o_size], (void*)r, N * sizeof(T));
+
+	};
+	template <size_t N>
+	void append(T(&&r)[N]) {
+
+		if (this->m_basePtr == r) return;
+
+		size_t o_size = this->m_size;
+		if (!doAlloc(this, this->m_size + N)) return;
+		memcpy((void*)&this->m_basePtr[o_size], (void*)r, N * sizeof(T));
+
+	};
 
 	//-----[Overloads]----------------------------------------------------------------------------------
 
-	dynArray& operator=(dynArray const& r) {
+	dynArray& operator=(dynArray& r) {
 
-		if (this->m_basePtr  == r.m_basePtr  &&
-			this->m_size   == r.m_size   &&
-			this->m_trueSize == r.m_trueSize &&
-			this->m_reserved == r.m_reserved &&
-			this->m_idx	   == r.m_idx
-		   ) 
-		{ return *this; }
-		if (this->m_basePtr) { free(this->m_basePtr); this->m_basePtr = nullptr; }
-		this->m_basePtr = (T*) calloc(r.m_trueSize, sizeof(T));
-		if (!this->m_basePtr) { printf("Memory allocation failed.\n"); exit(0); }
-		memcpy((void*)this->m_basePtr, (void*)r.m_basePtr, r.m_trueSize * sizeof(T));
-
-		this->m_size = r.m_size;
-		this->m_trueSize = r.m_trueSize;
-		this->m_reserved = r.m_reserved;
-		this->m_idx = r.m_idx;
-		return *this;
-
-	}
-	template<size_t N>
-	dynArray& operator=(T const(&r)[N]) {
-
-		if (!doAlloc(this, N)) return *this;
-		memcpy((void*)this->m_basePtr, (void*)r, N * sizeof(T));
-		this->m_size = N;
-		return *this;
-
-	}
-
-	//	Corrupts stack, don't use for now
-	/*friend dynArray operator+(dynArray& l, dynArray& r) {
-
-		dynArray<T> tmp;
-
-		if (!l.m_basePtr && !r.m_basePtr) return tmp;
-		else if (!l.m_basePtr) return r;
-		else if (!r.m_basePtr) return l;
-
-		tmp.append(l);
-		tmp.append(r);
-		return tmp;
-
-	}*/
-
-	dynArray& operator+=(dynArray const& r) {
-		
+		this->resize(0);
 		this->append(r);
 		return *this;
 
 	}
-	template<size_t N>
-	dynArray& operator+=(T const(&r)[N]) {
+	template <size_t N>
+	dynArray& operator=(T(&r)[N])  { 
 
+		this->resize(0);
+		this->append(r); 
+		return *this;
+
+	};
+	template <size_t N>
+	dynArray& operator=(T(&&r)[N]) { 
+
+		this->resize(0);
 		this->append(r);
 		return *this;
 
 	};
 
+	dynArray& operator+=(dynArray& r) { this->append(r); return *this; }
+	template <size_t N>
+	dynArray& operator+=(T(& r)[N])  { this->append(r); return *this; };
+	template <size_t N>
+	dynArray& operator+=(T(&& r)[N]) { this->append(r); return *this; };
+
 	T operator[](int i) const {
 
-		if ((unsigned)i >= this->m_size ||
-			(unsigned)i >= this->m_trueSize ||
-			((unsigned)i >= this->m_idx && this->m_reserved)
-			) { printf("Index is out of bounds at %i.\n", i); exit(0); }
+		assert(i < this->m_size);
 		return this->m_basePtr[i];
 
 	}
 	T& operator[](int i) {
 
-
-		if ((unsigned)i >= this->m_size ||
-			(unsigned)i >= this->m_trueSize ||
-			((unsigned)i >= this->m_idx && this->m_reserved)
-			) { printf("Index is out of bounds at %i.\n", i); exit(0); }
+		assert(i < this->m_size);
 		return this->m_basePtr[i];
 
 	}
@@ -305,83 +155,93 @@ public:
 
 	#ifdef DEBUG_EF
 
-	T* basePtr()				 { return this->m_basePtr; }
-	bool directAllocCall(uint n) { return doAlloc(this, n); }
-
-	uint fullSize()				 { return this->m_size; }
-	uint trueSize()				 { return this->m_trueSize; }
+	T* basePtr()				     { return this->m_basePtr;  }
+	bool directAllocCall(size_t n)   { return doAlloc(this, n); }
+	size_t trueSize()				 { return this->m_trueSize; }
 
 	#endif
 
 private:
 
 	//-----[Resize functions]---------------------------------------------------------------------------
-	
-	bool doAlloc(dynArray* arr, uint newLength, bool clamp = false) {
-		
-		if (newLength == 0) {
 
-			arr->m_size = 0;
-			if (clamp == false) return true;
+	bool doAlloc(dynArray* arr, size_t n_size) {
 
-			if (arr->m_basePtr) free(arr->m_basePtr);
-			arr->m_basePtr = nullptr;
-			arr->m_trueSize = 0;
+		//	Space will only be allocated if the new size forces the underlying array to grow.
+		//	If this is the case, it will grow to be 2x the size of the original.
+
+		if (arr->m_size == n_size) return true;
+		if (arr->m_size < n_size) {
+
+			if (!arr->m_basePtr) {
+
+				size_t n_trueSize = 1;
+				while (n_trueSize < n_size) n_trueSize = n_trueSize * 2;
+
+				arr->m_basePtr = (T*)calloc(n_trueSize, sizeof(T));
+				if (!arr->m_basePtr) return false;
+				arr->m_size = n_size;
+				arr->m_trueSize = n_trueSize;
+
+			}
+			else {
+
+				size_t n_trueSize = arr->m_trueSize;
+				while (n_trueSize < n_size) n_trueSize = n_trueSize * 2;
+
+				T* n_basePtr = nullptr;
+				n_basePtr = (T*)calloc(n_trueSize, sizeof(T));
+				if (!n_basePtr) return false;
+				memcpy((void*)n_basePtr, (void*)arr->m_basePtr, arr->m_trueSize * sizeof(T));
+
+				free(arr->m_basePtr); arr->m_basePtr = n_basePtr;
+				arr->m_size = n_size;
+				arr->m_trueSize = n_trueSize;
+
+			}
+		}
+
+		return true;
+
+	}
+
+	bool doResize(dynArray* arr, size_t n_size, bool clamp = false) {
+
+		//	Extension of doAlloc, allowing the size to be decreased.
+		//	If clamping is enabled, the size of the underlying array will also decrease.
+		//	Clamping may not work well with more complex classes.
+
+		if (n_size == 0) {
+
+			if (this->m_basePtr) free(this->m_basePtr);
+			this->m_size = 0; this->m_trueSize = 0;
 			return true;
 
 		}
-		if (arr->m_size > newLength && !arr->m_reserved) { 
+		if (arr->m_size == n_size) return true;
+		if (arr->m_size > n_size) {
 
 			if (clamp) {
 
-				T* tempPtr = nullptr; uint newTrueSize = arr->m_trueSize;
-				while (newTrueSize > newLength) newTrueSize = newTrueSize / 2;
-				newTrueSize = newTrueSize * 2;
-				tempPtr = (T*)calloc(newTrueSize, sizeof(T));
-				if (!tempPtr) { printf("Memory allocation failed.\n"); return false; }
+				size_t n_trueSize = arr->m_trueSize;
+				while (n_trueSize > n_size) n_trueSize = n_trueSize / 2;
+				n_trueSize = (n_trueSize != 0) ? n_trueSize * 2 : 1;
 
-				memcpy((void*)tempPtr, (void*)arr->m_basePtr, newTrueSize * sizeof(T));
-				free(arr->m_basePtr); arr->m_basePtr = tempPtr;
+				T* n_basePtr = nullptr;
+				n_basePtr = (T*)calloc(n_trueSize, sizeof(T));
+				if (!n_basePtr) return false;
+				memcpy((void*)n_basePtr, (void*)arr->m_basePtr, n_trueSize * sizeof(T));
 
-				arr->m_size = newLength;
-				arr->m_trueSize = newTrueSize;
-
-				return true;
-
-			}
-			arr->m_idx = newLength;
-			if(!arr->m_reserved) arr->m_size = newLength;
-			return true;
-		}
-		else if (arr->m_trueSize < newLength || !arr->m_basePtr) {
-
-			if (!arr->m_basePtr) { 
-
-				//	Create new array if the current base pointer is null
-				arr->m_trueSize = 1;
-				while (arr->m_trueSize < newLength) arr->m_trueSize = arr->m_trueSize * 2;
-				arr->m_basePtr = (T*)calloc(arr->m_trueSize, sizeof(T));
-				if (!arr->m_basePtr) { printf("Memory allocation failed.\n"); return false; }
-
-				arr->m_size = newLength;
-				return true;
+				free(arr->m_basePtr); arr->m_basePtr = n_basePtr;
+				arr->m_trueSize = n_trueSize;
 
 			}
 
-			//	Attempt to allocate new memory
-			T* tempPtr = nullptr; uint newTrueSize = arr->m_trueSize;
-			while (newTrueSize < newLength) newTrueSize = newTrueSize * 2;
-			tempPtr = (T*)calloc(newTrueSize, sizeof(T));
-			if (!tempPtr) { printf("Memory allocation failed.\n"); return false; }
-
-			memcpy((void*)tempPtr, (void*)arr->m_basePtr, newTrueSize * sizeof(T));
-			free(arr->m_basePtr); arr->m_basePtr = tempPtr;
-
-			arr->m_size = newLength;
-			arr->m_trueSize = newTrueSize;
+			arr->m_size = n_size;
 
 		}
-		
+		else return doAlloc(arr, n_size);
+
 		return true;
 
 	}
